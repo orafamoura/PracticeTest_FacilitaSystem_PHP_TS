0cc0606c7cabaabf456bd8ae09fdb841f7c94221
orafamoura <raphaelmvb2@gmail.com>
2024-07-29 17:18:07 -0300

    fix - TryCatch do db

--- index.php
+++ index.php
@@ -7,10 +7,18 @@ use src\services\TarefaService;
 use src\controllers\TarefaController;
 
 // Conectar ao MongoDB
-$client = new Client('mongodb://mongodb:27017');
+try {
+  $mongoUri = getenv('MONGO_URI') ?: 'mongodb://mongodb:27017';
+  $client = new Client($mongoUri);
+  $client->selectDatabase('teste_pratico_facilita_system')->command(['ping' => 1]); // Testa a conexão
+} catch (Exception $e) {
+  die('Erro ao conectar ao MongoDB: ' . $e->getMessage());
+}
+
 $collection = $client->selectCollection('teste_pratico_facilita_system', 'tarefas');
 $tarefaRepository = new TarefaRepository($collection);
 $tarefaService = new TarefaService($tarefaRepository);
 $tarefaController = new TarefaController($tarefaService);
 
+//Manipula a API
 $tarefaController->handleRequest();
